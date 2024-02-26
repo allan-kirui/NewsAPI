3fd66347116fb8dceb9f5f88380eaed2fc7e4c7e
allan-kirui <allan.kipkemoi.kirui@gmail.com>
2024-02-26 22:27:22 +0100

    Working POST request

--- locations/locations_api.py
+++ locations/locations_api.py
@@ -3,14 +3,15 @@ from uuid import UUID
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database.database import Database
-from locations import crud
+from locations import locations_crud, location_schemas
 
 router = APIRouter()
+
 
 # GETS
 @router.get("/{location_id}")
 def read_location(location_id: UUID, db: Session = Depends(Database().get_db)):
-    location = crud.get_location(db, location_id)
+    location = locations_crud.get_location(db, location_id)
     if location is None:
         raise HTTPException(status_code=404, detail="Location does not exist")
     return location
@@ -18,8 +19,15 @@ def read_location(location_id: UUID, db: Session = Depends(Database().get_db)):
 
 @router.get("/")
 def read_locations(skip: int = 0, limit: int = 10, db: Session = Depends(Database().get_db)):
-    locations = crud.get_locations(db, skip=skip, limit=limit)
+    locations = locations_crud.get_locations(db, skip=skip, limit=limit)
     return locations
 
 
-
+# POST
+@router.post("/")
+def create_location(location: location_schemas.LocationBase, db: Session = Depends(Database().get_db)):
+    try:
+        return locations_crud.create_location(db=db, name=location.name)
+    except Exception as e:
+        print(f"Error: {e}")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
