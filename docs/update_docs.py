b4fcfe91629765028dd8ee48a3aa3090b8f7ecf1
allan-kirui <allan.kipkemoi.kirui@gmail.com>
2024-02-27 22:10:02 +0100

    Test of prehook and disabled Github Actions

--- docs/update_docs.py
+++ docs/update_docs.py
@@ -1,6 +1,17 @@
+import os
 import requests
 
-response = requests.get("s3://openapi-docs-news/openapi.json")
+# Replace 'localhost:8000' with the actual host and port of your FastAPI application
+base_url = 'http://localhost:8000'
+openapi_path = '/openapi.json'
+docs_save_path = "./"
+url = base_url + openapi_path
 
-with open("docs/openapi.json", "w") as file:
-    file.write(response.text)
+try:
+    response = requests.get(url)
+    response.raise_for_status()
+    with open(os.path.join(docs_save_path, 'openapi.json'), 'wb') as f:
+        f.write(response.content)
+    print('OpenAPI JSON downloaded successfully.')
+except requests.exceptions.RequestException as e:
+    print(f'Error downloading OpenAPI JSON: {e}')
