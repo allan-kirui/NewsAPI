736b906a45e319ab584ac85a2e6d7727ae2dc8c9
allan-kirui <allan.kipkemoi.kirui@gmail.com>
2024-02-27 21:21:59 +0100

    Added aws s3 for file storage

--- docs/update_docs.py
+++ docs/update_docs.py
@@ -1,6 +1,6 @@
 import requests
 
-response = requests.get("http://localhost:8000/openapi.json")
+response = requests.get("s3://openapi-docs-news/openapi.json")
 
 with open("docs/openapi.json", "w") as file:
     file.write(response.text)
